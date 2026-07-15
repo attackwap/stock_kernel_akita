bb39961de1f71d33eaf805cb4b65252b84197ba8
attackwap <attackwap@users.noreply.github.com>
2026-07-15 12:32:55 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,1 +1,18 @@
 
+#!/bin/bash
+set -e
+
+exec > >(tee sync.log) 2>&1
+
+mkdir kernel
+cd kernel
+
+echo "Installing repo..."
+
+repo init \
+    -u https://android.googlesource.com/kernel/manifest \
+    -b android-gs-akita-6.1-android16
+
+repo sync -c -j$(nproc)
+
+echo "Done."
